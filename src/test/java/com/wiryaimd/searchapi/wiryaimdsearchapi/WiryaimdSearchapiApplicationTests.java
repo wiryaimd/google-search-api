7a4c73413133833482cfed0bd93c09f156c73cf0
wiryaimd <adnyasutha003@gmail.com>
2022-12-12 21:05:22 +0800

    setup project

---
+++ src/test/java/com/wiryaimd/searchapi/wiryaimdsearchapi/WiryaimdSearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiryaimd.searchapi.wiryaimdsearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiryaimdSearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
